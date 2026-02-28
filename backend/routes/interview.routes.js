01f40e0ffec1423575bf9a24724c9add89262dff
Your Name <your@email.com>
2026-02-28 20:03:32 +0200

    feat(api):add session, question and answer endpoints

--- backend/routes/interview.routes.js
+++ backend/routes/interview.routes.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-const { startInterview , answerQuestion } = require("../controllers/interview.controller");
+const { startInterview, getAllSessions,getSessionById,updateSessionStatus,deleteSession,getSessionQuestions,answerQuestion } = require("../controllers/interview.controller");
+
 router.post("/start", startInterview);
-router.post("/answer",answerQuestion);
+router.get("/",getAllSessions);
+router.post("/question/:questionId/answer", answerQuestion);
+router.get("/:id/questions",getSessionQuestions)
+router.get("/:id",getSessionById);
+router.put("/:id",updateSessionStatus);
+router.delete("/:id",deleteSession);
+
+
 module.exports=router;
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const interviewRoutes = require("./routes/interview.routes");
 
 const app=express();
@@ -8,9 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/interview", interviewRoutes);
-
-const PORT=5000;
-
+const PORT = process.env.PORT || 5000;
 app.listen(PORT,()=>{
     console.log(`server running on port ${PORT}`);
 });
