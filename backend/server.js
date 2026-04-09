ee17d5c8c30573bb32be1d86d8137dfcde082020
Your Name <your@email.com>
2026-04-09 15:28:24 +0300

    fix: add rout route

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,9 @@ const app=express();
 
 app.use(cors());
 app.use(express.json());
-
+app.get("/", (req, res) => {
+    res.send("API is running 🚀");
+  });
 app.use("/api/interview", interviewRoutes);
 app.use("/api/auth", authRoutes);
 const PORT = process.env.PORT || 5000;
